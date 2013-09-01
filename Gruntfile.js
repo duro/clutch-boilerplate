388cd28caee0cc4fb08382f8dc946aa72ba04fae
Adam Duro <adam@duromedia.com>
2013-09-01 16:59:25 -0700

    Updating to handle built in global statics folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,28 +44,64 @@ module.exports = function(grunt) {
       }
     },
 
-    clean: {
-      staticApps: ["public/statics/javascripts/apps"]
-    },
+    clean: ["public/statics"],
 
     hub: {
-      staticApps: {
+      buildStaticApps: {
         src: ['client/apps/*/Gruntfile.js'],
         tasks: ['build'],
       },
+      watchStaticApps: {
+        src: ['client/apps/*/Gruntfile.js'],
+        tasks: ['watch'],
+      },
+      buildGlobalStatics: {
+        src: ['client/statics/Gruntfile.js']
+      }
+    },
+
+    nodemon: {
+      dev: {
+        options: {
+          file: 'server/index.js',
+          ignoredFiles: ['README.md', 'node_modules/**'],
+          watchedExtensions: ['js'],
+          watchedFolders: ['server'],
+          delayTime: 1,
+          cwd: __dirname
+        }
+      }
+    },
+
+    concurrent: {
+      dev: {
+        tasks: ['nodemon:dev', 'hub:watchStaticApps'],
+        options: {
+          logConcurrentOutput: true
+        }
+      }
     }
   });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-hub');
+  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task.
   grunt.registerTask('default', ['jshint']);
 
   // Static build task
-  grunt.registerTask('build_statics', ['clean:staticApps', 'hub:staticApps']);
+  grunt.registerTask('build_statics', [
+    'clean', 'hub:buildGlobalStatics', 'hub:buildStaticApps'
+  ]);
+
+  // Dev task
+  grunt.registerTask('dev', ['concurrent:dev']);
 
 };
