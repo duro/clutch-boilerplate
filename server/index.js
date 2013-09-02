703210078ccdabb731aa17ee76fe4cb91e930fe9
Adam Duro <adam@duromedia.com>
2013-09-01 18:07:57 -0700

    Chaning boot loader so that it starts with one worker in dev mode

--- server/index.js
+++ server/index.js
@@ -20,27 +20,44 @@ cli = require('optimist')
   })
   .argv;
 
-// Code to run if we're in the master process
-if (cluster.isMaster) {
-  for (var i = 0; i < cli.workers; i += 1) {
-    cluster.fork();
-  }
-// Code to run if we're in a worker process
+if (cli.env === 'development') {
+
+  bootLocomotive(function(addr) {
+    console.log('Server started. [Env: %s] [Addr: %s] [Port: %s]', cli.env, addr.address, addr.port);
+  });
+
 } else {
+
+  // Code to run if we're in the master process
+  if (cluster.isMaster) {
+    for (var i = 0; i < cli.workers; i += 1) {
+      cluster.fork();
+    }
+  // Code to run if we're in a worker process
+  } else {
+    console.log('Worker %s: Starting boot sequence...', cluster.worker.id);
+    bootLocomotive(function(addr){
+      console.log('Worker %s: Server started. [Env: %s] [Addr: %s] [Port: %s]', cluster.worker.id, cli.env, addr.address, addr.port);
+    })
+  }
+
+}
+
+function bootLocomotive(cb) {
   // Boot Locomotive.
   // Calls back with a fully configured express instance.
-  console.log('Worker %s: Starting boot sequence...', cluster.worker.id);
   locomotive.boot(__dirname, cli.env, function(err, express) {
     if (err) throw err;
 
     // Start Express Server
     express.listen(cli.port, function() {
       var addr = this.address();
-      console.log('Worker %s: Server started. [Env: %s] [Addr: %s] [Port: %s]', cluster.worker.id, cli.env, addr.address, addr.port);
+      cb(addr);
     });
   });
 }
 
+// Cluster exit handler
 cluster.on('exit', function (worker) {
 
   // Replace the dead worker,
